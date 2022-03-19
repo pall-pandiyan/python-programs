78f7cdb29123954080d19560f7bb862ba562a764
Pall Pandiyan <pallpandiyan@gmail.com>
2022-03-19 21:07:21 +0530

    end of today's work

---
+++ basicPandas.py
@@ -0,0 +1,7 @@
+import pandas as pd
+s1=pd.Series([10,20,30,40])
+print(s1)
+print(type(s1))
+s2=pd.DataFrame({"Name":["Apple","Orenges","Banana"],"Value":[150,210,55]})
+print(s2)
+print(type(s2))
---
+++ fifa.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+fifa=pd.read_csv('player-data.csv')
+print(fifa.head())
