b5f0f5f2270bf933dba48d91d5b809433b0b0a87
Pall Pandiyan <pallpandiyan@gmail.com>
2022-03-19 13:05:15 +0530

    First commit

---
+++ basicNumpy.py
@@ -0,0 +1,9 @@
+import numpy as np
+n1=np.array([10,20,30,40])
+print("n1 array is",n1)
+n2=np.array([[1,2,3],[3,4,5],[5,6,7]])
+print("n2 array is")
+print(n2)
+n3=np.zeros((2,3))
+print("n3 array is")
+print(n3)
