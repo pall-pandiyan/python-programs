5615eab5fca38389067bcaa5354fbfe875460783
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-01 20:41:30 +0530

    added explanation for mysql operations.

---
+++ mysqlDemo.py
@@ -0,0 +1,56 @@
+import mysql.connector
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="coder",
+    password="coder",
+    database="coderdb"
+)
+
+# print(mydb)
+mycursor = mydb.cursor()
+
+# mycursor.execute("CREATE DATABASE coderdb")
+# mycursor.execute("SHOW DATABASES")
+
+#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
+#mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+
+#mycursor.execute("SHOW TABLES")
+
+#[print (x) for x in mycursor]
+
+#for x in mycursor:
+#    print(x)
+
+'''
+print("Enter the number of lines to be inputed:", end=" ")
+n = int(input().strip())
+val = []
+
+for i in range(n):
+    print("Enter the name to insert:", end=" ")
+    name = input().strip()
+
+    print("Enter the address to insert:", end=" ")
+    address = input().strip()
+
+    val.append((name,address))
+
+
+if len(name)<255 and len(address)<255 :
+    sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
+    # val = (name,address)
+    mycursor.executemany(sql, val)
+    mydb.commit()
+    print(mycursor.rowcount, "rows entered! ID:", mycursor.lastrowid)
+'''
+
+sql = "SELECT name, address FROM customers WHERE address like %s ORDER BY name DESC"
+adr = ( "%way%", )
+
+mycursor.execute(sql, adr)
+print(mycursor.fetchall())
+
+#[print(x) for x in mycursor.fetchall()]
+#for x in mycursor.fetchall():
+#    print (x)
