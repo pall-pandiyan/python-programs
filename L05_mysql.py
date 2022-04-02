92b36863724725ce1f3af34bea7623208519217a
Pall Pandiyan <pallpandiyan@gmail.com>
2022-04-02 15:26:19 +0530

    added mysql details.

--- L05_mysql.py
+++ L05_mysql.py
@@ -1,28 +1,36 @@
 import mysql.connector
+
 mydb = mysql.connector.connect(
     host="localhost",
     user="coder",
     password="coder",
     database="coderdb"
 )
-
-# print(mydb)
 mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE coderdb")
-# mycursor.execute("SHOW DATABASES")
+mycursor.execute("SHOW DATABASES")
 
-#mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")
-#mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+if ('coderdb',) in mycursor:
+    print("Database already exists!")
+    #mydb._database = "coderdb"
+    #mycursor.execute("SHOW TABLES")
+    #[print(x) for x in mycursor]
 
-#mycursor.execute("SHOW TABLES")
 
-#[print (x) for x in mycursor]
+else:
+    mycursor.execute("CREATE DATABASE coderdb")
+    mycursor.execute("CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))")
 
+    mydb.commit()
+
+"""
+mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")
+mycursor.execute("SHOW TABLES")
+
+[print (x) for x in mycursor]
 #for x in mycursor:
 #    print(x)
 
-'''
 print("Enter the number of lines to be inputed:", end=" ")
 n = int(input().strip())
 val = []
@@ -36,14 +44,13 @@ for i in range(n):
 
     val.append((name,address))
 
-
 if len(name)<255 and len(address)<255 :
     sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
     # val = (name,address)
     mycursor.executemany(sql, val)
     mydb.commit()
     print(mycursor.rowcount, "rows entered! ID:", mycursor.lastrowid)
-'''
+
 
 sql = "SELECT name, address FROM customers WHERE address like %s ORDER BY name DESC"
 adr = ( "%way%", )
@@ -54,3 +61,4 @@ print(mycursor.fetchall())
 #[print(x) for x in mycursor.fetchall()]
 #for x in mycursor.fetchall():
 #    print (x)
+"""
