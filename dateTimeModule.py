63130a1cd82f54a22b1e836147ba38b85e6ee0dd
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-05 22:27:31 +0530

    added detailed demo datetime module

--- dateTimeModule.py
+++ dateTimeModule.py
@@ -1,14 +1,19 @@
 import datetime as dt
 
 date = dt.datetime.now()
+
 day = "0"+str(date.day) if date.day<10 else str(date.day)
 month = "0"+str(date.month) if date.month<10 else str(date.month)
 year = str(date.year)
 formattedDate = day + "/" + month + "/" + year
-print(date)
-print(formattedDate)
+
+print("date:", date)
+print("date.strftime():", date.strftime("%d/%m/%Y"))
+print("formattedDate:", formattedDate)
+
 
 # the output will be...
 
-# 2022-05-05 22:14:30.253327
-# 05/05/2022
+# date: 2022-05-05 22:26:48.035445
+# date.strftime(): 05/05/2022
+# formattedDate: 05/05/2022
