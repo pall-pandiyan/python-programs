d812fd998566584b42c02b8d6aa9058e3e1bb436
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-06 12:48:17 +0530

    added a simple demo to generate a random rumber

--- randomModule.py
+++ randomModule.py
@@ -1,3 +1,14 @@
 import random
 
-print(random)
+# random module have uniform method which will take two parameters (start and end limit) and returns a random float value, we can further use round or int to refine our results. (rounding 1.5 will return 2, but int method will return 1).
+
+print("random.uniform(1,10):", random.uniform(1,10))
+print("int(random.uniform(100, 150)):", int(random.uniform(100, 150)))
+print("round(random.uniform(100, 150)):", round(random.uniform(100, 150)))
+
+
+# the output will be...
+
+# random.uniform(1,10): 5.879905617184167
+# int(random.uniform(100, 150)): 116
+# round(random.uniform(100, 150)): 140
