037b69eced0716bf5c1321479005982410f70000
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-06 12:24:44 +0530

    demo of round()

---
+++ roundFunction.py
@@ -0,0 +1,16 @@
+import math
+
+# round method have two parameters the first is the number to be rounded, the second optional parameter is limitation of rounding.
+print("round(math.pi, 2):", round(math.pi, 2))
+print("round(math.pi, 4):", round(math.pi, 4))
+print("round(11):", round(11))
+print("round(123.1):", round(123.1))
+print("round(14.547):", round(14.547))
+
+# the output will be...
+
+# round(math.pi, 2): 3.14
+# round(math.pi, 4): 3.1416
+# round(11): 11
+# round(123.1): 123
+# round(14.547): 15
