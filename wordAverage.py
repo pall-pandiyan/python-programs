4f6aad7c64563d2991bd09f13f3813fa9fb4efb9
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-11 18:00:36 +0530

    solved

--- wordAverage.py
+++ wordAverage.py
@@ -2,15 +2,25 @@ def wordAverage(lst):
     sum = 0
     for item in lst:
         sum = sum + len(item)
+    return (sum/len(lst))
 
-    return (sum//len(lst))
 
 if __name__ == "__main__":
-    text1 = "India will be 5 trillian economy by 2025"
-    text2="India will surpass US economy by 2040"
+    text1 = "Python is fun!!!"
+    text2 = "Python is easy to learn, use and understand!"
 
     lst1 = list(text1.split())
     lst2 = list(text2.split())
 
-    print("Average for text1:", wordAverage(lst1))
-    print("Average for text2:", wordAverage(lst2))
+    print("text1:", text1)
+    print("text2:", text2)
+
+    print("Word average for text1:", wordAverage(lst1))
+    print("Word average for text2:", wordAverage(lst2))
+
+# the output will be...
+
+# text1: Python is fun!!!
+# text2: Python is easy to learn, use and understand!
+# Word average for text1: 4.666666666666667
+# Word average for text2: 4.625
