074f5760aeb99ce72b0d0f88a6da3e364c38e0bc
Pall Pandiyan.S <pallpandiyan@gmail.com>
2025-07-26 01:07:14 +0530

    optimized code added

--- code optimization/001_sum_of_two_elements.py
+++ code optimization/001_sum_of_two_elements.py
@@ -1,7 +1,8 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 
 
 def sum_of_two_elements(numbers: List[int], target: int) -> Tuple[int]:
+    """
     # This is O(n2) complexity - because of two for loops.
 
     size: int = len(numbers)
@@ -10,9 +11,29 @@ def sum_of_two_elements(numbers: List[int], target: int) -> Tuple[int]:
             if numbers[x] + numbers[y] == target:
                 return (x, y)
     return None
+    """
+    size: int = len(numbers)
+    visited_numbers: Dict[int, int] = {}
+
+    for i in range(size):
+        diff: int = target - numbers[i]
+
+        # print(f"{visited_numbers=}")
+        # print(f"{number=}")
+        # print(f"{diff=}")
+
+        # we cannot skip this for some edge cases - numbers = [2,2,3,4], target=4
+        # if the element is duplicated, just skip the iteration.
+        # if numbers[i] in visited_numbers:
+        # continue
+
+        if diff in visited_numbers:
+            return (i, visited_numbers[diff])
+
+        visited_numbers[numbers[i]] = i
 
 
-numbers: List[int] = [2, 3, 4, 5, 8]
-target: int = 7
+numbers: List[int] = [2, 2, 3, 4, 5, 8]
+target: int = 4
 print(f"numbers: {numbers} and target: {target}")
 print(f"sum_of_two_elements: {sum_of_two_elements(numbers,target)}")
